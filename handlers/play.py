8f528e2b36f31ad31b559ab1f5a9d9c65c39a54d
Raflyskut <94258234+Raflyskut@users.noreply.github.com>
2021-11-14 06:59:00 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -586,7 +586,7 @@ async def play(_, message: Message):
         )
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/fa2cdb8a14a26950da711.png"
+        thumb_name = "https://telegra.ph/file/2a7533f21bf36924560ea.jpg"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         message.from_user.first_name
